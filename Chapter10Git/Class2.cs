09f2a643016d1d1b27d9283dd93011faa5d8272e
McMakster <maximmirzinskij@gmail.com>
2018-04-14 12:49:07 +0300

    Just the start

---
+++ Chapter10Git/Class2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Chapter10Git
+{
+    class Class2
+    {
+        int var = 10;
+        bool yes = false;
+    }
+}
